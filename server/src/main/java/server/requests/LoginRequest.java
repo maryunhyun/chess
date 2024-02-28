0f5df6783eec9ffc3865c9c5c0863423d2561065
maryunhyun <maryunhyun@gmail.com>
2024-02-27 20:59:23 -0700

    init(phase0)

--- server/src/main/java/server/requests/LoginRequest.java
+++ server/src/main/java/server/requests/LoginRequest.java
@@ -15,10 +15,6 @@ public class LoginRequest {
         return username;
     }
 
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
     public String getPassword() {
         return password;
     }
--- shared/src/main/java/chessPieces/Pawn.java
+++ shared/src/main/java/chessPieces/Pawn.java
@@ -6,8 +6,8 @@ import java.util.HashSet;
 
 public class Pawn {
     public HashSet<ChessMove> pawnMoves(ChessBoard board, ChessPosition myPosition, HashSet<ChessMove> movesCollection) {
-        ChessPosition [] chessPositions7 = new ChessPosition[10];
-        ChessMove [] chessMoves7 = new ChessMove[10];
+        ChessPosition[] chessPositions7 = new ChessPosition[10];
+        ChessMove[] chessMoves7 = new ChessMove[10];
 
         int z = 0;
         int i = myPosition.getRow();
@@ -15,46 +15,44 @@ public class Pawn {
         String black = "b";
         String white = "w";
 
-        if (myPosition.getRow() == 7 && board.getBoardSquares()[myPosition.getRow()-1][myPosition.getColumn()-1].getTeamColor() == ChessGame.TeamColor.BLACK) {
-            for (ChessMove chessMove: firstMoveBlack(i,j,z,board,myPosition,chessPositions7,chessMoves7,movesCollection)) {
+        if (myPosition.getRow() == 7 && board.getBoardSquares()[myPosition.getRow() - 1][myPosition.getColumn() - 1].getTeamColor() == ChessGame.TeamColor.BLACK) {
+            for (ChessMove chessMove : firstMoveBlack(i, j, z, board, myPosition, chessPositions7, chessMoves7, movesCollection)) {
                 movesCollection.add(chessMove);
             }
 
-        }
-        else if (myPosition.getRow() == 2 && board.getBoardSquares()[myPosition.getRow()-1][myPosition.getColumn()-1].getTeamColor() == ChessGame.TeamColor.WHITE) {
-            for (ChessMove chessMove: firstMoveWhite(i,j,z,board,myPosition,chessPositions7,chessMoves7,movesCollection)) {
+        } else if (myPosition.getRow() == 2 && board.getBoardSquares()[myPosition.getRow() - 1][myPosition.getColumn() - 1].getTeamColor() == ChessGame.TeamColor.WHITE) {
+            for (ChessMove chessMove : firstMoveWhite(i, j, z, board, myPosition, chessPositions7, chessMoves7, movesCollection)) {
                 movesCollection.add(chessMove);
             }
         }
         //make sure moving forward in correct direction depending on color
-        else if (board.getBoardSquares()[myPosition.getRow()-1][myPosition.getColumn()-1].getTeamColor() == ChessGame.TeamColor.BLACK){
-            for (ChessMove chessMove: normalMoveBlack(i,j,z,board,myPosition,chessPositions7,chessMoves7,movesCollection)) {
+        else if (board.getBoardSquares()[myPosition.getRow() - 1][myPosition.getColumn() - 1].getTeamColor() == ChessGame.TeamColor.BLACK) {
+            for (ChessMove chessMove : normalMoveBlack(i, j, z, board, myPosition, chessPositions7, chessMoves7, movesCollection)) {
                 movesCollection.add(chessMove);
             }
-        }
-
-        else if (board.getBoardSquares()[myPosition.getRow()-1][myPosition.getColumn()-1].getTeamColor() == ChessGame.TeamColor.WHITE){
-            for (ChessMove chessMove: normalMoveWhite(i,j,z,board,myPosition,chessPositions7,chessMoves7,movesCollection)) {
+        } else if (board.getBoardSquares()[myPosition.getRow() - 1][myPosition.getColumn() - 1].getTeamColor() == ChessGame.TeamColor.WHITE) {
+            for (ChessMove chessMove : normalMoveWhite(i, j, z, board, myPosition, chessPositions7, chessMoves7, movesCollection)) {
                 movesCollection.add(chessMove);
             }
         }
         return movesCollection;
     }
-    HashSet<ChessMove> firstMoveBlack(int i, int j,int z, ChessBoard board, ChessPosition myPosition, ChessPosition[] chessPositions7, ChessMove[] chessMoves7, HashSet<ChessMove> movesCollection) {
+
+    HashSet<ChessMove> firstMoveBlack(int i, int j, int z, ChessBoard board, ChessPosition myPosition, ChessPosition[] chessPositions7, ChessMove[] chessMoves7, HashSet<ChessMove> movesCollection) {
         //move like first move for black piece
         //move two spaces
         i = i - 2;
         if (j < 9 && j > 0 && i < 9 && i > 0) {
-            twoSpacesMove(i,j,z,board,myPosition,chessPositions7,chessMoves7,movesCollection);
+            twoSpacesMove(i, j, z, board, myPosition, chessPositions7, chessMoves7, movesCollection);
             //checking for enemies in diagonal
             j--;
             if (j > 0) {
-                diagonalMove(i,j,z,board,myPosition,chessPositions7,chessMoves7,movesCollection);
+                diagonalMove(i, j, z, board, myPosition, chessPositions7, chessMoves7, movesCollection);
 
             }
-            j = j+2;
+            j = j + 2;
             if (j < 9) {
-                diagonalMove(i,j,z,board,myPosition,chessPositions7,chessMoves7,movesCollection);
+                diagonalMove(i, j, z, board, myPosition, chessPositions7, chessMoves7, movesCollection);
 
             }
             i = myPosition.getRow();
@@ -64,41 +62,37 @@ public class Pawn {
         i = myPosition.getRow();
         i = i - 1;
         if (j < 9 && j > 0 && i < 9 && i > 0) {
-            if (board.getBoardSquares()[i - 1][j - 1] == null) {
-                chessPositions7[z] = new ChessPosition(i, j);
-                chessMoves7[z] = new ChessMove(myPosition, chessPositions7[z], null);
-                movesCollection.add(chessMoves7[z]);
-                z++;
-            }
+            normalMove(i,j,z,board,myPosition,chessPositions7,chessMoves7,movesCollection);
             //checking for enemies in diagonal
             j--;
             if (j > 0) {
-                diagonalMove(i,j,z,board,myPosition,chessPositions7,chessMoves7,movesCollection);
+                diagonalMove(i, j, z, board, myPosition, chessPositions7, chessMoves7, movesCollection);
             }
-            j = j+2;
+            j = j + 2;
             if (j < 9) {
-                diagonalMove(i,j,z,board,myPosition,chessPositions7,chessMoves7,movesCollection);
+                diagonalMove(i, j, z, board, myPosition, chessPositions7, chessMoves7, movesCollection);
             }
             i = myPosition.getRow();
             j = myPosition.getColumn();
         }
         return movesCollection;
     }
-    HashSet<ChessMove> firstMoveWhite(int i, int j,int z, ChessBoard board, ChessPosition myPosition, ChessPosition[] chessPositions7, ChessMove[] chessMoves7, HashSet<ChessMove> movesCollection) {
+
+    HashSet<ChessMove> firstMoveWhite(int i, int j, int z, ChessBoard board, ChessPosition myPosition, ChessPosition[] chessPositions7, ChessMove[] chessMoves7, HashSet<ChessMove> movesCollection) {
         //move like first move for white piece
 
         //move two spaces
         i = i + 2;
         if (j < 9 && j > 0 && i < 9 && i > 0) {
-            twoSpacesMove(i,j,z,board,myPosition,chessPositions7,chessMoves7,movesCollection);
+            twoSpacesMove(i, j, z, board, myPosition, chessPositions7, chessMoves7, movesCollection);
             //checking for enemies in diagonal
             j++;
             if (j < 9) {
-                diagonalMove(i,j,z,board,myPosition,chessPositions7,chessMoves7,movesCollection);
+                diagonalMove(i, j, z, board, myPosition, chessPositions7, chessMoves7, movesCollection);
             }
-            j = j-2;
+            j = j - 2;
             if (j > 0) {
-                diagonalMove(i,j,z,board,myPosition,chessPositions7,chessMoves7,movesCollection);
+                diagonalMove(i, j, z, board, myPosition, chessPositions7, chessMoves7, movesCollection);
             }
             i = myPosition.getRow();
             j = myPosition.getColumn();
@@ -107,70 +101,40 @@ public class Pawn {
         i = myPosition.getRow();
         i = i + 1;
         if (j < 9 && j > 0 && i < 9 && i > 0) {
-            if (board.getBoardSquares()[i - 1][j - 1] == null) {
-                chessPositions7[z] = new ChessPosition(i, j);
-                chessMoves7[z] = new ChessMove(myPosition, chessPositions7[z], null);
-                movesCollection.add(chessMoves7[z]);
-                z++;
-            }
+
+            normalMove(i,j,z,board,myPosition,chessPositions7,chessMoves7,movesCollection);
             //checking for enemies in diagonal
             j++;
             if (j < 9) {
-                diagonalMove(i,j,z,board,myPosition,chessPositions7,chessMoves7,movesCollection);
+                diagonalMove(i, j, z, board, myPosition, chessPositions7, chessMoves7, movesCollection);
             }
-            j = j-2;
+            j = j - 2;
             if (j > 0) {
-                diagonalMove(i,j,z,board,myPosition,chessPositions7,chessMoves7,movesCollection);
+                diagonalMove(i, j, z, board, myPosition, chessPositions7, chessMoves7, movesCollection);
             }
             i = myPosition.getRow();
             j = myPosition.getColumn();
         }
         return movesCollection;
     }
-    HashSet<ChessMove> normalMoveBlack(int i, int j,int z, ChessBoard board, ChessPosition myPosition, ChessPosition[] chessPositions7, ChessMove[] chessMoves7, HashSet<ChessMove> movesCollection) {
+
+    HashSet<ChessMove> normalMoveBlack(int i, int j, int z, ChessBoard board, ChessPosition myPosition, ChessPosition[] chessPositions7, ChessMove[] chessMoves7, HashSet<ChessMove> movesCollection) {
         i--;
         if (j < 9 && j > 0 && i < 9 && i > 0) {
             if (board.getBoardSquares()[i - 1][j - 1] == null) {
-                //promotion if reach end of board
-                if (i == 1) {
-                    promotion(i,j,z,board,myPosition,chessPositions7,chessMoves7,movesCollection);
-                }
-                else {
-                    chessPositions7[z] = new ChessPosition(i, j);
-                    chessMoves7[z] = new ChessMove(myPosition, chessPositions7[z], null);
-                    movesCollection.add(chessMoves7[z]);
-                    z++;
-                }
+                promotionEndofBoardBlack(i,j,z,board,myPosition,chessPositions7,chessMoves7,movesCollection);
             }
             //checking for enemies in diagonal
             j--;
-            if (board.getBoardSquares()[i-1][j-1] != null) {
+            if (board.getBoardSquares()[i - 1][j - 1] != null) {
                 if (board.getBoardSquares()[i - 1][j - 1].getTeamColor() != board.getBoardSquares()[myPosition.getRow() - 1][myPosition.getColumn() - 1].getTeamColor()) {
-                    //promotion if reach end of board
-                    if (i == 1) {
-                        promotion(i,j,z,board,myPosition,chessPositions7,chessMoves7,movesCollection);
-                    }
-                    else {
-                        chessPositions7[z] = new ChessPosition(i, j);
-                        chessMoves7[z] = new ChessMove(myPosition, chessPositions7[z], null);
-                        movesCollection.add(chessMoves7[z]);
-                        z++;
-                    }
+                    promotionEndofBoardBlack(i,j,z,board,myPosition,chessPositions7,chessMoves7,movesCollection);
                 }
             }
-            j = j+2;
-            if (board.getBoardSquares()[i-1][j-1] != null) {
+            j = j + 2;
+            if (board.getBoardSquares()[i - 1][j - 1] != null) {
                 if (board.getBoardSquares()[i - 1][j - 1].getTeamColor() != board.getBoardSquares()[myPosition.getRow() - 1][myPosition.getColumn() - 1].getTeamColor()) {
-                    //promotion if reach end of board
-                    if (i == 1) {
-                        promotion(i,j,z,board,myPosition,chessPositions7,chessMoves7,movesCollection);
-                    }
-                    else {
-                        chessPositions7[z] = new ChessPosition(i, j);
-                        chessMoves7[z] = new ChessMove(myPosition, chessPositions7[z], null);
-                        movesCollection.add(chessMoves7[z]);
-                        z++;
-                    }
+                    promotionEndofBoardBlack(i,j,z,board,myPosition,chessPositions7,chessMoves7,movesCollection);
                 }
             }
             i = myPosition.getRow();
@@ -179,51 +143,27 @@ public class Pawn {
         }
         return movesCollection;
     }
-    HashSet<ChessMove> normalMoveWhite(int i, int j,int z, ChessBoard board, ChessPosition myPosition, ChessPosition[] chessPositions7, ChessMove[] chessMoves7, HashSet<ChessMove> movesCollection) {
+
+    HashSet<ChessMove> normalMoveWhite(int i, int j, int z, ChessBoard board, ChessPosition myPosition, ChessPosition[] chessPositions7, ChessMove[] chessMoves7, HashSet<ChessMove> movesCollection) {
         i++;
         if (j < 9 && j > 0 && i < 9 && i > 0) {
             if (board.getBoardSquares()[i - 1][j - 1] == null) {
-                //promotion if reach end of board
-                if (i == 8) {
-                    promotion(i,j,z,board,myPosition,chessPositions7,chessMoves7,movesCollection);
-                }
-                else {
-                    chessPositions7[z] = new ChessPosition(i, j);
-                    chessMoves7[z] = new ChessMove(myPosition, chessPositions7[z], null);
-                    movesCollection.add(chessMoves7[z]);
-                    z++;
-                }
+                promotionEndofBoardWhite(i,j,z,board,myPosition,chessPositions7,chessMoves7,movesCollection);
             }
             //checking for enemies in diagonal
             j++;
             if (j < 9) {
                 if (board.getBoardSquares()[i - 1][j - 1] != null) {
                     if (board.getBoardSquares()[i - 1][j - 1].getTeamColor() != board.getBoardSquares()[myPosition.getRow() - 1][myPosition.getColumn() - 1].getTeamColor()) {
-                        //promotion if reach end of board
-                        if (i == 8) {
-                            promotion(i,j,z,board,myPosition,chessPositions7,chessMoves7,movesCollection);
-                        } else {
-                            chessPositions7[z] = new ChessPosition(i, j);
-                            chessMoves7[z] = new ChessMove(myPosition, chessPositions7[z], null);
-                            movesCollection.add(chessMoves7[z]);
-                            z++;
-                        }
+                        promotionEndofBoardWhite(i,j,z,board,myPosition,chessPositions7,chessMoves7,movesCollection);
                     }
                 }
             }
-            j = j-2;
+            j = j - 2;
             if (j > 0) {
                 if (board.getBoardSquares()[i - 1][j - 1] != null) {
                     if (board.getBoardSquares()[i - 1][j - 1].getTeamColor() != board.getBoardSquares()[myPosition.getRow() - 1][myPosition.getColumn() - 1].getTeamColor()) {
-                        //promotion if reach end of board
-                        if (i == 8) {
-                            promotion(i,j,z,board,myPosition,chessPositions7,chessMoves7,movesCollection);
-                        } else {
-                            chessPositions7[z] = new ChessPosition(i, j);
-                            chessMoves7[z] = new ChessMove(myPosition, chessPositions7[z], null);
-                            movesCollection.add(chessMoves7[z]);
-                            z++;
-                        }
+                        promotionEndofBoardWhite(i,j,z,board,myPosition,chessPositions7,chessMoves7,movesCollection);
                     }
                 }
             }
@@ -232,7 +172,8 @@ public class Pawn {
         }
         return movesCollection;
     }
-    HashSet<ChessMove> diagonalMove(int i, int j,int z, ChessBoard board, ChessPosition myPosition, ChessPosition[] chessPositions7, ChessMove[] chessMoves7, HashSet<ChessMove> movesCollection) {
+
+    HashSet<ChessMove> diagonalMove(int i, int j, int z, ChessBoard board, ChessPosition myPosition, ChessPosition[] chessPositions7, ChessMove[] chessMoves7, HashSet<ChessMove> movesCollection) {
         if (board.getBoardSquares()[i - 1][j - 1] != null) {
             if (board.getBoardSquares()[i - 1][j - 1].getTeamColor() != board.getBoardSquares()[myPosition.getRow() - 1][myPosition.getColumn() - 1].getTeamColor()) {
                 chessPositions7[z] = new ChessPosition(i, j);
@@ -244,7 +185,8 @@ public class Pawn {
         return movesCollection;
 
     }
-    HashSet<ChessMove> twoSpacesMove(int i, int j,int z, ChessBoard board, ChessPosition myPosition, ChessPosition[] chessPositions7, ChessMove[] chessMoves7, HashSet<ChessMove> movesCollection) {
+
+    HashSet<ChessMove> twoSpacesMove(int i, int j, int z, ChessBoard board, ChessPosition myPosition, ChessPosition[] chessPositions7, ChessMove[] chessMoves7, HashSet<ChessMove> movesCollection) {
 
         if (board.getBoardSquares()[i - 1][j - 1] == null && board.getBoardSquares()[i][j - 1] == null) {
             chessPositions7[z] = new ChessPosition(i, j);
@@ -254,7 +196,8 @@ public class Pawn {
         }
         return movesCollection;
     }
-    HashSet<ChessMove> promotion(int i, int j,int z, ChessBoard board, ChessPosition myPosition, ChessPosition[] chessPositions7, ChessMove[] chessMoves7, HashSet<ChessMove> movesCollection) {
+
+    HashSet<ChessMove> promotion(int i, int j, int z, ChessBoard board, ChessPosition myPosition, ChessPosition[] chessPositions7, ChessMove[] chessMoves7, HashSet<ChessMove> movesCollection) {
 
         //get chosen promotion option and insert
         chessPositions7[z] = new ChessPosition(i, j);
@@ -273,6 +216,41 @@ public class Pawn {
         chessMoves7[z] = new ChessMove(myPosition, chessPositions7[z], ChessPiece.PieceType.ROOK);
         movesCollection.add(chessMoves7[z]);
         z++;
+        return movesCollection;
+    }
+
+    HashSet<ChessMove> normalMove(int i, int j, int z, ChessBoard board, ChessPosition myPosition, ChessPosition[] chessPositions7, ChessMove[] chessMoves7, HashSet<ChessMove> movesCollection) {
+        if (board.getBoardSquares()[i - 1][j - 1] == null) {
+            chessPositions7[z] = new ChessPosition(i, j);
+            chessMoves7[z] = new ChessMove(myPosition, chessPositions7[z], null);
+            movesCollection.add(chessMoves7[z]);
+            z++;
+        }
+        return movesCollection;
+    }
+    HashSet<ChessMove> promotionEndofBoardWhite(int i, int j, int z, ChessBoard board, ChessPosition myPosition, ChessPosition[] chessPositions7, ChessMove[] chessMoves7, HashSet<ChessMove> movesCollection) {
+        //promotion if reach end of board
+        if (i == 8) {
+            promotion(i, j, z, board, myPosition, chessPositions7, chessMoves7, movesCollection);
+        } else {
+            chessPositions7[z] = new ChessPosition(i, j);
+            chessMoves7[z] = new ChessMove(myPosition, chessPositions7[z], null);
+            movesCollection.add(chessMoves7[z]);
+            z++;
+        }
+        return movesCollection;
+    }
+
+    HashSet<ChessMove> promotionEndofBoardBlack(int i, int j, int z, ChessBoard board, ChessPosition myPosition, ChessPosition[] chessPositions7, ChessMove[] chessMoves7, HashSet<ChessMove> movesCollection) {
+        //promotion if reach end of board
+        if (i == 1) {
+            promotion(i, j, z, board, myPosition, chessPositions7, chessMoves7, movesCollection);
+        } else {
+            chessPositions7[z] = new ChessPosition(i, j);
+            chessMoves7[z] = new ChessMove(myPosition, chessPositions7[z], null);
+            movesCollection.add(chessMoves7[z]);
+            z++;
+        }
         return movesCollection;
     }
 }
